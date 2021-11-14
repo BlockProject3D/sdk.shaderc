7359c6ae7d81d841dae293cc8f85e822ebc5cf42
Yuri Edward <yuri6037@outlook.com>
2021-11-15 00:29:08 +0100

    Added initial version of thread pool

--- shaderc/src/main.rs
+++ shaderc/src/main.rs
@@ -26,6 +26,8 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+mod thread_pool;
+
 use std::ffi::OsString;
 
 use clap::clap_app;
