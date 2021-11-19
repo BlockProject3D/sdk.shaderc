812600d8fdd1385c17785b7a6755d7feecde81e4
Yuri Edward <yuri6037@outlook.com>
2021-11-19 22:15:33 +0100

    Fixed some issues in shader_to_sal helper

--- shaderc/src/targets/basic/shader_to_sal.rs
+++ shaderc/src/targets/basic/shader_to_sal.rs
@@ -27,13 +27,13 @@
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 use std::cmp::Ordering;
-use std::collections::BTreeSet;
+use std::collections::{BTreeSet, HashMap};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::ops::Deref;
 use bpx::shader::Stage;
 use log::warn;
-use sal::ast::tree::{BlendfuncStatement, PipelineStatement, Property, PropertyType, Statement, Struct};
+use sal::ast::tree::{Attribute, BlendfuncStatement, PipelineStatement, Property, PropertyType, Statement, Struct};
 use sal::utils::auto_lexer_parser;
 use crate::options::{Args, Error, ShaderUnit};
 use crate::targets::basic::preprocessor::BasicPreprocessor;
@@ -45,7 +45,8 @@ pub struct ShaderToSal
 {
     pub name: String,
     pub strings: Vec<rglslang::shader::Part>,
-    pub statements: Vec<Statement>
+    pub statements: Vec<Statement>,
+    pub stage: Stage
 }
 
 fn shader_sal_stage<T: BufRead>(name: String, content: T, args: &Args) -> Result<ShaderToSal, Error>
@@ -53,11 +54,12 @@ fn shader_sal_stage<T: BufRead>(name: String, content: T, args: &Args) -> Result
     let mut result = ShaderToSal {
         strings: Vec::new(),
         statements: Vec::new(),
-        name: name.clone()
+        name: name.clone(),
+        stage: Stage::Vertex
     };
     let mut preprocessor = BasicPreprocessor::new(&args.libs);
     preprocessor::run(content, &mut preprocessor)?;
-    let stage = preprocessor.stage.unwrap_or_else(|| {
+    result.stage = preprocessor.stage.unwrap_or_else(|| {
         warn!("No shader stage specified in shader file, assuming this is a vertex shader by default");
         Stage::Vertex
     });
@@ -93,6 +95,7 @@ pub fn load_shader_to_sal(unit: &ShaderUnit, args: &Args) -> Result<ShaderToSal,
 pub struct OrderedProp
 {
     pub inner: Property,
+    pub slot: u32,
     index: u64
 }
 
@@ -141,15 +144,64 @@ impl Ord for OrderedProp
     }
 }
 
+pub struct Slot<T>
+{
+    pub inner: T,
+    pub slot: u32
+}
+
+impl<T> Slot<T>
+{
+    pub fn new(t: T) -> Self
+    {
+        Self {
+            inner: t,
+            slot: 0
+        }
+    }
+}
+
 pub struct StmtDecomposition
 {
+    pub root_constants_layout: Option<Struct>,
     pub root_constants: BTreeSet<OrderedProp>, //Root constants/push constants, emulated by global uniform buffer in GL targets
     pub outputs: BTreeSet<OrderedProp>, //Fragment shader outputs/render target outputs
-    pub objects: Vec<Property>, //Samplers and textures
-    pub cbuffers: Vec<Struct>,
+    pub objects: Vec<Slot<Property>>, //Samplers and textures
+    pub cbuffers: Vec<Slot<Struct>>,
     pub vformat: Option<Struct>,
     pub pipeline: Option<PipelineStatement>,
     pub blendfuncs: Vec<BlendfuncStatement>
+}
+
+impl StmtDecomposition
+{
+    pub fn extend(&mut self, other: StmtDecomposition) -> Result<(), Error>
+    {
+        self.root_constants.extend(other.root_constants);
+        self.outputs.extend(other.outputs);
+        self.objects.extend(other.objects);
+        self.cbuffers.extend(other.cbuffers);
+        if self.root_constants_layout.is_some() && other.root_constants_layout.is_some() {
+            return Err(Error::new("multiple definitions of the root constant buffer"));
+        }
+        if self.root_constants_layout.is_none() {
+            self.root_constants_layout = other.root_constants_layout;
+        }
+        if self.vformat.is_some() && other.vformat.is_some() {
+            return Err(Error::new("multiple definitions of the vertex format"));
+        }
+        if self.vformat.is_none() {
+            self.vformat = other.vformat;
+        }
+        if self.pipeline.is_some() && other.pipeline.is_some() {
+            return Err(Error::new("multiple definitions of the pipeline"));
+        }
+        if self.pipeline.is_none() {
+            self.pipeline = other.pipeline;
+        }
+        self.blendfuncs.extend(other.blendfuncs);
+        Ok(())
+    }
 }
 
 pub fn decompose_statements<'a>(stmts: Vec<Statement>) -> Result<StmtDecomposition, Error>
@@ -162,18 +214,21 @@ pub fn decompose_statements<'a>(stmts: Vec<Statement>) -> Result<StmtDecompositi
     let mut cbuffers= Vec::new();
     let mut vformat = None;
     let mut pipeline = None;
+    let mut root_constants_layout = None;
     let mut blendfuncs= Vec::new();
     let mut insert_root_const = |p: Property| {
         assert!(root_constants.insert(OrderedProp {
             inner: p,
-            index: rcounter
+            index: rcounter,
+            slot: 0
         }));
         rcounter += 1;
     };
     let mut insert_output = |p: Property| {
         assert!(outputs.insert(OrderedProp {
             inner: p,
-            index: ocounter
+            index: ocounter,
+            slot: 0
         }));
         ocounter += 1;
     };
@@ -184,10 +239,20 @@ pub fn decompose_statements<'a>(stmts: Vec<Statement>) -> Result<StmtDecompositi
                     PropertyType::Scalar(_) => insert_root_const(p),
                     PropertyType::Vector(_) => insert_root_const(p),
                     PropertyType::Matrix(_) => insert_root_const(p),
-                    _ => objects.push(p)
+                    _ => objects.push(Slot::new(p))
                 };
             }
-            Statement::ConstantBuffer(s) => cbuffers.push(s),
+            Statement::ConstantBuffer(inner) => {
+                if let Some(attr) = &inner.attr {
+                    if let Attribute::Order(o) = attr {
+                        if *o == 0 {
+                            root_constants_layout = Some(inner);
+                            continue;
+                        }
+                    }
+                }
+                cbuffers.push(Slot::new(inner))
+            },
             Statement::Output(o) => insert_output(o),
             Statement::VertexFormat(s) => {
                 if vformat.is_some() {
@@ -206,6 +271,7 @@ pub fn decompose_statements<'a>(stmts: Vec<Statement>) -> Result<StmtDecompositi
         }
     }
     Ok(StmtDecomposition {
+        root_constants_layout,
         root_constants,
         objects,
         outputs,
