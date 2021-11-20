c576596cfacb903929308e56236f66090bc85d9a
Yuri Edward <yuri6037@outlook.com>
2021-11-20 15:08:45 +0100

    Initial support for reusing symbols across stages

--- shaderc/src/targets/basic/sal_compiler.rs
+++ shaderc/src/targets/basic/sal_compiler.rs
@@ -29,10 +29,10 @@
 use std::collections::{HashMap, HashSet};
 use bp3d_threads::{ScopedThreadManager, ThreadPool};
 use bpx::shader::Stage;
-use log::{debug, info, warn};
-use sal::ast::tree::{Attribute, PropertyType, Struct};
+use log::{debug, error, info, warn};
+use sal::ast::tree::{Attribute, Property, PropertyType, Struct};
 use crate::options::{Args, Error};
-use crate::targets::basic::{decompose_statements, load_shader_to_sal, StmtDecomposition};
+use crate::targets::basic::{decompose_statements, load_shader_to_sal, Slot, StmtDecomposition};
 
 pub struct DecomposedShader
 {
@@ -113,68 +113,73 @@ pub fn merge_stages(shaders: Vec<DecomposedShader>) -> Result<HashMap<Stage, Sha
     Ok(map)
 }
 
-//TODO: Handle/test the case where the same constant buffer/constant would be used across multiple stages
 pub fn relocate_bindings<F: FnMut(BindingType, Option<u32>, u32) -> u32>(stages: &mut HashMap<Stage, ShaderStage>, mut func: F)
 {
+    let mut map = HashMap::new();
     stages.iter_mut().for_each(|(_, v)| {
         for v in &mut v.statements.cbuffers {
-            if let Some(attr) = &v.inner.attr {
-                if let Attribute::Order(slot) = attr {
-                    /*if !cbuf_slots.insert(*slot) {
-                        warn!("Possible duplicate of binding slot {} when relocating constant buffer '{}'", slot, v.inner.name);
-                    }*/
-                    v.slot = func(BindingType::CBuf, Some(*slot), v.slot);
-                    continue;
+            let mut cbuf_func = || {
+                if let Some(attr) = &v.inner.attr {
+                    if let Attribute::Order(slot) = attr {
+                        return func(BindingType::CBuf, Some(*slot), v.slot);
+                    }
                 }
+                func(BindingType::CBuf, None, v.slot)
+            };
+            let fsk;
+            if let Some(slot) = map.get(&v.inner.name) {
+                fsk = *slot;
+            } else {
+                fsk = cbuf_func();
+                map.insert(&v.inner.name, fsk);
             }
-            v.slot = func(BindingType::CBuf, None, v.slot);
+            v.slot = fsk;
         }
         for v in &mut v.statements.objects {
-            if v.inner.ptype != PropertyType::Sampler {
+            let mut prop_func = |t: BindingType| {
                 if let Some(attr) = &v.inner.pattr {
                     if let Attribute::Order(slot) = attr {
-                        v.slot = func(BindingType::Sampler, Some(*slot), v.slot);
-                        continue;
+                        return func(BindingType::Sampler, Some(*slot), v.slot);
                     }
                 }
-                v.slot = func(BindingType::Sampler, None, v.slot);
+                func(BindingType::Sampler, None, v.slot)
+            };
+            let fsk;
+            if let Some(slot) = map.get(&v.inner.pname) {
+                fsk = *slot;
             } else {
-                if let Some(attr) = &v.inner.pattr {
-                    if let Attribute::Order(slot) = attr {
-                        v.slot = func(BindingType::Texture, Some(*slot), v.slot);
-                        continue;
-                    }
-                }
-                v.slot = func(BindingType::Texture, None, v.slot);
+                fsk = match v.inner.ptype {
+                    PropertyType::Sampler => prop_func(BindingType::Sampler),
+                    _ => prop_func(BindingType::Texture)
+                };
+                map.insert(&v.inner.pname, fsk);
             }
+            v.slot = fsk;
         }
     });
-    /*stages.iter_mut().for_each(|(_, v)| {
-        v.statements.cbuffers.iter_mut().for_each(|v| {
-            if v.slot == 0 {
-                while cbuf_slots.contains(&cur_cbuf_slot) {
-                    cur_cbuf_slot += 1;
-                }
-                v.slot = cur_cbuf_slot;
-                cur_cbuf_slot += 1;
-            }
-        });
-    });*/
 }
 
 pub fn test_bindings<F: FnMut(BindingType, u32) -> bool>(stages: &HashMap<Stage, ShaderStage>, mut func: F) -> Result<(), Error>
 {
+    let mut map = HashMap::new();
     for (stage, v) in stages {
         if v.statements.root_constants_layout.is_some() && !func(BindingType::CBuf, 0) {
             return Err(Error::from(format!("multiple definitions of binding {} in stage {:?}", 0, stage)));
         }
         for slot in &v.statements.cbuffers {
+            if map.contains_key(&slot.inner.name) {
+                continue;
+            }
             if !func(BindingType::CBuf, slot.slot) {
                 warn!("Constant buffer '{}' is attempting to relocate to {} which is already in use!", slot.inner.name, slot.slot);
                 return Err(Error::from(format!("multiple definitions of binding {} in stage {:?}", 0, stage)));
             }
+            map.insert(&slot.inner.name, slot.slot);
         }
         for slot in &v.statements.objects {
+            if map.contains_key(&slot.inner.pname) {
+                continue;
+            }
             if slot.inner.ptype != PropertyType::Sampler {
                 if !func(BindingType::Sampler, slot.slot) {
                     warn!("Sampler '{}' is attempting to relocate to {} which is already in use!", slot.inner.pname, slot.slot);
@@ -185,6 +190,27 @@ pub fn test_bindings<F: FnMut(BindingType, u32) -> bool>(stages: &HashMap<Stage,
                     warn!("Texture '{}' is attempting to relocate to {} which is already in use!", slot.inner.pname, slot.slot);
                     return Err(Error::from(format!("multiple definitions of binding {} in stage {:?}", 0, stage)));
                 }
+            }
+            map.insert(&slot.inner.pname, slot.slot);
+        }
+    }
+    Ok(())
+}
+
+pub fn test_symbols(stages: &HashMap<Stage, ShaderStage>) -> Result<(), Error>
+{
+    for (stage, v) in stages {
+        let mut set = HashSet::new();
+        for v in &v.statements.cbuffers {
+            if !set.insert(&v.inner.name) {
+                error!("Multiple definitions of symbol '{}'", v.inner.name);
+                return Err(Error::new("multiple definitions of the same symbol"))
+            }
+        }
+        for v in &v.statements.objects {
+            if !set.insert(&v.inner.pname) {
+                error!("Multiple definitions of symbol '{}'", v.inner.pname);
+                return Err(Error::new("multiple definitions of the same symbol"))
             }
         }
     }
