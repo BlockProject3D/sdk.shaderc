353a77a8dee38ac592fc64c21d80bfe90adc74c8
Yuri Edward <yuri6037@outlook.com>
2021-12-31 22:41:37 +0100

    Added note to remember trying a visitor API for SAL

--- sal/src/lib.rs
+++ sal/src/lib.rs
@@ -26,6 +26,8 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+//TODO: Use visitor in SAL
+
 pub mod ast;
 pub mod lexer;
 pub mod parser;
